8ac20747a7ea22ddc7e4e45729ac0977450c952e
vincentlunc <xiaopeng@live.unc.edu>
2018-11-19 14:09:56 -0500

    Finish xml conversion

--- mail2xml.py
+++ mail2xml.py
@@ -4,6 +4,7 @@ import time
 import re
 
 # tags
+start           = '<add>\n<doc>\n'
 f_message_id    = '<field name="message_id">'
 f_date          = '<field name="date">'
 f_from          = '<field name="from">'
@@ -12,12 +13,10 @@ f_subject       = '<field name="subject">'
 f_from_name     = '<field name="from_name">'
 f_to_name       = '<field name="to_name">'
 f_content       = '<field name="content">'
-
 close           = '</field>\n'
 
-
+debug = []
 def txt2xml(filepath):
-
     with open (filepath, "r") as f:
         lines = f.readlines()
         temp_dict = {}
@@ -29,37 +28,62 @@ def txt2xml(filepath):
                 
                 if ":" in line:
                     split_line = line.strip().split(":", 1)
+                    split_line[1] = split_line[1].strip()
                 else:
                     split_line[1] += re.sub(r"[\s+]", ' ', line)
                 temp_dict[split_line[0]] = split_line[1]
             else:
-                print("email content processing here!")
+                # Email content processing
                 content = True
                 try:
                     temp_dict["content"] += line
                 except:
                     temp_dict["content"] = ""
                     temp_dict["content"] += line
-        # next to do: convert dictionary to xml
-        return temp_dict
+        # Convert dictionary to xml
+        global start, f_message_id, f_date, f_from, f_to, f_subject, f_from_name, f_to_name, f_content, debug
+        xml = ""
+        try:
+            _message_id = f_message_id + temp_dict["Message-ID"] + close
+            _date = f_date + temp_dict["Date"] + close
+            _from = f_from + temp_dict["From"] + close
+            try:
+                _to = f_to + temp_dict["To"] + close
+            except KeyError:
+                temp_dict["To"] = ""
+                _to = f_to + temp_dict["To"] + close
+            
+            _subject = f_subject + temp_dict["Subject"] + close
+            _from_name = f_from_name + temp_dict["X-From"] + close
+            _to_name = f_to_name + temp_dict["X-To"] + close
+            _content = f_content + temp_dict["content"] + close
+            xml = start + _message_id + _date + _from + _to + _subject + _from_name + _to_name + _content
+        except Exception as e:
+            debug.append((filepath, e))
+        return xml
             
             
         
-
 def main(rootdir="./maildir", targetdir="./xmldir"):
     count = 0
-    for subdir, dirs, files in os.walk(rootdir):
+    cwd = os.getcwd()
+    for subdir, dirs, files in os.walk(rootdir):        
         for file in files:
             if file != ".DS_Store":
-                print("addf")
                 xml = txt2xml(os.path.join(subdir, file))
-                print(xml)
                 if not os.path.exists(targetdir):
                     os.makedirs(targetdir)
                     # make name and save
-                # return xml
-            else:
-                print(file)
+                    # next thing to do: save converted xml with name
+                else:
+                    # make name and save
+                    save_name = os.path.join(subdir.split("./maildir/", 1)[1], file).replace("/", "_") + "xml"
+                    save_name = os.path.join(targetdir, save_name)
+                    with open(save_name, "w") as f:
+                        f.write(xml)
+                        print("finished writing into "+ save_name)
+
+    print(debug)
     return
 
 main()
